c0d883f840a737a668e93e60c78c6cd8925ee606
Aman Mender <aman@Amans-iMac.local>
2018-02-26 17:49:52 +0100

    Updated typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,6 @@
 import Typography from "typography";
-import kirkhamTheme from "typography-theme-kirkham";
+import funstonTheme from "typography-theme-funston";
 
-const typography = new Typography(kirkhamTheme);
+const typography = new Typography(funstonTheme);
 
 export default typography;
