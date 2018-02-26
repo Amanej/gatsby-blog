71c2a3e1fa396f4d7475064bca519739636979c9
Aman <aman.mender@tegningtil3d.no>
2018-02-26 19:33:23 +0100

    Style fix

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = ({ }) => {
   return (
-    <footer style={{position:"absolute", bottom:"0", textAlign: "center",width: "100%",padding: "5px",fontStyle: "italic"}}>
+    <footer style={{position:"fixed", bottom:"0", textAlign: "center",width: "100%",padding: "5px",fontStyle: "italic",background: 'white'}}>
       <div>
         <a href="https://www.gatsbyjs.org/">Built with GatsbyJS</a>
       </div>
