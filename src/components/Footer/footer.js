64ce00f4ce771fd143f69ee9759dad32979c50d6
Aman Mender <aman@Amans-iMac.local>
2018-02-26 17:38:28 +0100

    Added a bottom link

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ const Footer = ({ }) => {
   return (
     <footer style={{position:"absolute", bottom:"0", textAlign: "center",width: "100%",padding: "5px",fontStyle: "italic"}}>
       <div>
-        Built with GatsbyJS
+        <a href="https://www.gatsbyjs.org/">Built with GatsbyJS</a>
       </div>
       <div>
       </div>
