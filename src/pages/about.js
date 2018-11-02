4d76e122ec0dbe277a4b5a26fbf37ef87eb57db1
Aman <aman.mender@tegningtil3d.no>
2018-11-02 04:32:09 -0700

    Added linkedin link

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,6 +9,7 @@ export default ({data}) => (
       Hi, my name is Aman. I am 28 years old developer entrepreneur, and aspiring tinker.
       I want to tinker with the universe, and everything it has to offer.
     </p>
+    <a href="https://www.linkedin.com/in/aman-mender-92681b13/">LinkedIn</a>
   </div>
 );
 
