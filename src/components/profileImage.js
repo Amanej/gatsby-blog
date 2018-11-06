9a83afe0af4417d18c69f7a2db48e76be2d79a23
Aman <aman.mender@tegningtil3d.no>
2018-11-06 00:11:40 -0800

    Styled blog

--- src/components/profileImage.js
+++ src/components/profileImage.js
@@ -1,9 +1,10 @@
 import React from "react";
 import aman from '../../static/aman.jpg';
+import aboutStyles from "../components/css/about.css"
 
 function ProfileImage() {
   // Import result is the URL of your image
-  return <img src={aman} alt="Aman" />;
+  return <img className={aboutStyles.profileImage} src={aman} alt="Aman" />;
 }
 
 export default ProfileImage;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ProfileImage from '../components/profileImage';
+import aboutStyles from "../components/css/about.css"
+//console.log("About styles ",aboutStyles);
 
 export default ({data}) => (
   <div>
-    <h1>{data.site.siteMetadata.title}</h1>
+    <h1>About</h1>
     <ProfileImage></ProfileImage>
-    <p>
+    <p className={aboutStyles.aboutText}>
       Hi, my name is Aman. I am 28 years old developer entrepreneur, and aspiring tinker.
       I want to tinker with the universe, and everything it has to offer.
     </p>
-    <a href="https://www.linkedin.com/in/aman-mender-92681b13/">LinkedIn</a>
+    <a className={aboutStyles.aboutLink} href="https://www.linkedin.com/in/aman-mender-92681b13/">LinkedIn</a>
   </div>
 );
 
